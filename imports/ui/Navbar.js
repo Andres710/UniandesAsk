5e07271854e30e8516a5e6e174c45f865660db60
Andrés Felipe López <af.lopezf@uniandes.edu.co>
2018-10-04 01:35:48 -0500

    Super avance, falta ponerlo bonito

---
+++ imports/ui/Navbar.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+
+import AccountsUIWrapper from './AccountsUIWrapper.js';
+import { NavLink } from 'react-router-dom';
+
+export default class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      onChange: props.onChange
+    };
+  }
+  
+  render() {
+    return (
+      <nav className="navbar navbar-expand-sm bg-light navbar-light">
+
+        <a className="navbar-brand" href="/">Uniandes Ask</a>
+  
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <a className="nav-link" href="new">Crear pregunta</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Link 2</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Link 3</a>
+          </li>
+          <li className="nav-item">
+            <AccountsUIWrapper />
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+}
