dcc48eb045a712b0173bbb31928082717cd0a62a
sebastian <js.millan10@uniandes.edu.co>
2018-10-07 01:55:21 -0500

    Un poco de diseño para iniciar la pantalla de inicio, mañana la termino con tranquilidad

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,11 +1,9 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import { Meteor } from 'meteor/meteor';
-import { withTracker } from 'meteor/react-meteor-data';
-import './styles/Question.css';
-
-import { Questions } from '../api/questions.js';
-import { Answers } from '../api/answers.js';
+import {Meteor} from 'meteor/meteor';
+import {withTracker} from 'meteor/react-meteor-data';
+import {Questions} from '../api/questions.js';
+import {Answers} from '../api/answers.js';
 
 import Navbar from './Navbar.js';
 import Question from './Question.js';
@@ -37,23 +35,23 @@ class App extends Component {
 
   renderFilterTags() {
     return this.state.filterTags.map((tag) => (
-      <li key={tag}>{tag} </li>
+      <button type="button" className="btn btn-dark col-2 botonTag" key={tag}>{tag}</button>
     ));
   }
 
   renderQuestions() {
     let filteredQuestions = this.props.questions;
     let currentTags = this.state.filterTags;
-    if(currentTags.length > 0) {
+    if (currentTags.length > 0) {
       filteredQuestions = filteredQuestions.filter(question => {
-        for(let i = 0; i < currentTags.length; i++) {
-          if(!question.tags.includes(currentTags[i])){
+        for (let i = 0; i < currentTags.length; i++) {
+          if (!question.tags.includes(currentTags[i])) {
             return false;
           }
         }
         return true;
       });
-    } 
+    }
     return filteredQuestions.map((question) => (
       <Question key={question._id} question={question}/>
     ));
@@ -61,43 +59,37 @@ class App extends Component {
 
   render() {
     let filters;
-    if(this.state.filterTags.length > 0) {
+    if (this.state.filterTags.length > 0) {
       filters = (
-        <div className="container">
-          <ul className="horizontal-list">
-            <li>Tags: </li>
+        <div className="row">
             {this.renderFilterTags()}
-          </ul>
-          <br/>
         </div>
       );
     }
-
     return (
-      <div>
+      <div className="backo">
         <Navbar/>
         <br/>
-        <br/>
-
         <div className="container">
-          <div className="container">
-            <h3>
-              Busca por tags:
-            </h3>
-            <form className="new-tag-filter" onSubmit={this.addTagFilter.bind(this)}>
-              <input
-                type="text"
-                ref="textFilterTag"
-                placeholder="Agrega un tag"
-                size="20"
-              />
+          <div className="jumbotron" id="jumboFiltro">
+            <form onSubmit={this.addTagFilter.bind(this)}>
+              <div className="form-row">
+                <div className="col-4" id="filtra_label">
+                  <label>Filtra por Tags:</label>
+                </div>
+                <div className="col-8">
+                  <input type="text" className="form-control form-control-lg" id="colFormLabelLg" ref="textFilterTag"
+                         placeholder="Agrega un tag"/>
+                </div>
+              </div>
             </form>
+            <br/>
+              {filters}
           </div>
-
-          {filters}
-
+          <div className="jumbotron">
+            <h2>Preguntas</h2>
           {this.renderQuestions()}
-          
+          </div>
         </div>
 
       </div>
@@ -109,7 +101,7 @@ export default withTracker(() => {
   Meteor.subscribe('questions');
 
   return {
-    questions: Questions.find({}, { sort: { createdAt: -1 } }).fetch(),
+    questions: Questions.find({}, {sort: {createdAt: -1}}).fetch(),
     currentUser: Meteor.user(),
   };
 })(App);
--- imports/ui/Navbar.js
+++ imports/ui/Navbar.js
@@ -1,37 +1,29 @@
 import React, {Component} from 'react';
-import { Meteor } from 'meteor/meteor';
+import {Meteor} from 'meteor/meteor';
 import AccountsUIWrapper from './AccountsUIWrapper.js';
-import { NavLink } from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 
 export default class Navbar extends Component {
   constructor(props) {
     super(props);
-    
+
     this.state = {
       onChange: props.onChange
     };
   }
-  
+
   render() {
     return (
-      <nav className="navbar navbar-expand-sm bg-light navbar-light">
-
-        <a className="navbar-brand" href="/">Uniandes Ask</a>
-  
-        <ul className="navbar-nav">
-          {!!Meteor.user()?<li className="nav-item">
-            <a className="nav-link" href="new">Crear pregunta</a>
-          </li>:''}
-          <li className="nav-item">
-            <a className="nav-link" href="#">Link 2</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">Link 3</a>
-          </li>
-          <li className="nav-item">
-            <AccountsUIWrapper />
-          </li>
-        </ul>
+      <nav className="navbar navbar-dark bg-dark sticky-top">
+        <a className="navbar-brand nav-link hvr-underline-from-center" href="/">Uniandes Ask</a>
+        <div className="row" id="">
+            {!!Meteor.user() ? <div className="col nav-item">
+              <a className="nav-link hvr-underline-from-center" id="navLink" href="new">Crear pregunta</a>
+            </div> : ''}
+            <div className="col nav-item">
+              <AccountsUIWrapper/>
+            </div>
+        </div>
       </nav>
     );
   }
